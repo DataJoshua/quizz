d1f3110bd7e0eb014ae9d00384e6d2db46f4b3b6
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-03-08 14:37:42 +0400

    added at least 2 options per question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,7 +6,8 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :options, allow_destroy: true, reject_if: :all_blank
 
   validates :content, presence: true
-  validate :validate_at_least_one_correct_option
+  # validate :validate_at_least_one_correct_option
+  validate :validate_at_least_two_options_are_present
 
   private
 
@@ -14,5 +15,11 @@ class Question < ApplicationRecord
     return if options.any?(&:correct)
 
     errors.add(:questions, "At least one question must be correct")
+  end
+
+  def validate_at_least_two_options_are_present
+    return if options.size >= 2
+
+    errors.add(:questions, "You have to create at least 2 options for each question")
   end
 end
