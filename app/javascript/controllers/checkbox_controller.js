4d40ed87d8086040cb243f0aae8ee5ad63870234
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-03-14 20:36:35 +0400

    made the hability to create multioption questions, checkbox stimulus controller refactored to handle single choice and multi choice questions

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -2,8 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["checkbox"]
+  static values = {
+    correctOptions : Number
+  }
 
-  handleSingleAnswer(e) {
+  execute(e) {
+    if(this.correctOptionsValue === 1) {
+      this.#handleSingleAnswer(e)
+    }
+  }
+
+  #handleSingleAnswer(e) {
     this.checkboxTargets
                         .filter(val => val !== e.target)
                         .forEach(val => { val.checked = false })
