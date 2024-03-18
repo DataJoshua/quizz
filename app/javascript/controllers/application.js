98d626072ec4badadcc6d3331d752fe0c056ad94
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-03-18 17:18:52 +0400

    added flatpickr

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import { Modal } from "tailwindcss-stimulus-components"
+import Flatpickr from "stimulus-flatpickr"
 
 const application = Application.start()
 
+application.register("flatpickr", Flatpickr)
 application.register("modal", Modal)
 
 application.debug = false
