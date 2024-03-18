5b471dce915a6e764f3a67905a5968669867f714
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-03-18 23:02:46 +0400

    optimization of queries when creating a quiz execute (Teachers::Quizzes::Controller)

--- app/interactors/quizzes/create/execute.rb
+++ app/interactors/quizzes/create/execute.rb
@@ -6,19 +6,27 @@ module Quizzes
       delegate :quiz_params, to: :context
 
       def call
-        context.fail!(error: quiz_errors) unless quiz.save
+        context.fail!(error: quiz_errros) unless quiz.failed_instances.empty?
 
-        context.quiz = quiz
+        context.quiz = Quiz.includes(:questions)
+                           .find_by(id: quiz.ids.first)
       end
 
       private
 
       def quiz
-        @quiz ||= Quiz.new(quiz_params)
+        quiz_obj = Quiz.new(quiz_params)
+
+        @quiz ||= Quiz.import [quiz_obj], recursive: true, track_validation_failures: true, all_or_none: true
       end
 
-      def quiz_errors
-        quiz.errors.full_messages.join(", ")
+      def quiz_errros
+        quiz.failed_instances
+            .first
+            .last
+            .errors
+            .full_messages
+            .join(", ")
       end
     end
   end
