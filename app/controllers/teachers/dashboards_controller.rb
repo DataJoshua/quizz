8b6261e458511fe1e74990dce6a056eb75b8ba9e
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-05-01 16:28:20 +0400

    fix

--- app/controllers/teachers/dashboards_controller.rb
+++ app/controllers/teachers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 module Teachers
   class DashboardsController < ApplicationController
     expose :raw_quizzes, -> { current_user.quizzes.order(created_at: :desc) }
-    expose :pagginated_quizzes, -> { pagy(raw_quizzes, items: 5) }
-    expose :quizzes, -> { pagginated_quizzes.last }
+    expose :paginated_quizzes, -> { pagy(raw_quizzes, items: 5) }
+    expose :quizzes, -> { paginated_quizzes.last }
 
     def show; end
   end
