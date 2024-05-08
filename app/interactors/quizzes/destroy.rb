01d03334dd24c40a7d8d7d181bba738af21e7fa3
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-05-08 17:26:57 +0400

    added sidekiq job and ActionCable broadcast

--- app/interactors/quizzes/destroy.rb
+++ app/interactors/quizzes/destroy.rb
@@ -2,6 +2,17 @@ module Quizzes
   class Destroy
     include Interactor::Organizer
 
+    delegate :quiz, to: :context
+
     organize Quizzes::Destroy::Execute
+
+    after do
+      if quiz.undiscarded?
+        ActionCable.server.broadcast("quiz_channel", {
+          action: "deleted",
+          msg: "An Quiz #{quiz.name} has been deleted"
+        })
+      end
+    end
   end
 end
--- app/jobs/quiz_job.rb
+++
@@ -1,7 +0,0 @@
-class QuizJob < ApplicationJob
-  queue_as :default
-
-  def perform(quiz:)
-    quiz.discard
-  end
-end
